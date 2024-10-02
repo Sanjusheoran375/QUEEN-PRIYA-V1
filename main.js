d14c512624f4fcfaeea5f9bddeb8b59a09292ac9
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-10-03 04:44:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 //Instagram: Prexzy.ay
 //Telegram: t.me/Prexzyvilla_tech
 //GitHub: @Prexzybooster
-//WhatsApp: +2349159895444
+//WhatsApp: +919728893187
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@prexzyvilla
 
 require('./settings')
